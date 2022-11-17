ce4f815a8deed063581221047a7337333374a6b6
Pear-White <lhwawhite@gmail.com>
2022-11-17 22:23:35 +0800

    backup

--- getData.js
+++ getData.js
@@ -4,10 +4,10 @@ const { metadata, pngOptions } = require('./imageOptions.js')
 const fs = require('node:fs')
 const path = require('node:path')
 
-const dirname = './images/fullscreen'
+const dirname = './images/20221117'
 let idx = fs.readdirSync(dirname).length
 
-setInterval(() => {
+setTimeout(() => {
 	screenshot().then((img) => {
 		const filename = idx.toString().padStart(3, '0') + '.PNG'
 		const writePath = path.join(dirname, filename)
@@ -15,7 +15,8 @@ setInterval(() => {
 			.withMetadata(metadata)
 			.png(pngOptions)
 			.resize(1920, 1080)
+			.extract({ left: 220, top: 920, width: 1240, height: 160 })
 			.toFile(writePath)
 		idx++
 	})
-}, 20000)
+}, 4000)
